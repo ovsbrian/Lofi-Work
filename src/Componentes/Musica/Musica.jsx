c46772d44fa745ee1b8a6e02a5fea9586d721c34
kevin-dorado <kevindorado2811@gmail.com>
2023-10-05 16:28:02 -0300

    text white test1

--- src/Componentes/Musica/Musica.jsx
+++ src/Componentes/Musica/Musica.jsx
@@ -1,8 +1,8 @@
 export const Musica = () => {
   return (
     <>
-      <div className="bg-black w-full h-1/6">
-        asd
+      <div className="bg-black w-full h-1/6 text-white">
+        asd HOlaaaaaaaa
       </div>
     </>
   );
--- src/Componentes/Pomodoro/Pomodoro.jsx
+++ src/Componentes/Pomodoro/Pomodoro.jsx
@@ -1,7 +1,7 @@
 export const Pomodoro = () => {
   return (
     <>
-      <div className="h-5/6 bg-gray-900">dasdasd</div>
+      <div className="h-5/6 bg-gray-900 text-white">dasdasd</div>
     </>
   );
 };
