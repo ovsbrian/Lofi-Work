c2087ab6d1d709794cd216773c9c8bafcd8dacb6
ovsbrian <ovsbrian@gmail.com>
2023-10-11 17:19:45 -0300

    ToDo Finished

--- src/utilities/Task.jsx
+++ src/utilities/Task.jsx
@@ -1,1 +1,17 @@
- 
+import { FaCheck, FaTrash } from 'react-icons/fa';
+export const Task = ({text,id}) =>{
+
+    return(
+    <li className="flex justify-between items-center p-4 bg-white shadow rounded-md mb-2">
+        <span>{text}</span>
+        <div>
+            <button id={id+"check"} className="text-green-600 hover:text-green-800 mr-2">
+                <FaCheck />
+            </button>
+            <button id={id+"trash"}  className="text-red-600 hover:text-red-800">
+                <FaTrash />
+            </button>
+        </div>
+    </li>
+    )
+}
