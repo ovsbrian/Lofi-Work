2a19e5dc50b8c54aa6701381662c1fc8a4c7e550
ovsbrian <ovsbrian@gmail.com>
2023-10-11 16:56:28 -0300

    add music and gif

--- src/utilities/Task.jsx
+++ src/utilities/Task.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  import { FaCheck, FaTrash } from 'react-icons/fa';
 
 export const Task = ({ Text }) => {
@@ -12,6 +13,28 @@ export const Task = ({ Text }) => {
           <FaTrash />
         </button>
       </div>
+=======
+
+import { FaCheck, FaTrash } from 'react-icons/fa';
+
+
+
+
+export const Task = ({text,id}) =>{
+
+
+    return(
+    <li className="flex justify-between items-center p-4 bg-white shadow rounded-md mb-2">
+        <span>{text}</span>
+        <div>
+            <button id={id+"check"} className="text-green-600 hover:text-green-800 mr-2">
+                <FaCheck />
+            </button>
+            <button id={id+"trash"}  className="text-red-600 hover:text-red-800">
+                <FaTrash />
+            </button>
+        </div>
+>>>>>>> 9e058c3e90b24596868f93b243c84d1adba2e14a
     </li>
   );
 };
