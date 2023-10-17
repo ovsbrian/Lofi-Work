16839ef632b612fb6f00cd619b2320d749736606
ovsbrian <ovsbrian@gmail.com>
2023-10-17 19:28:50 -0300

    add btn mute

--- src/Componentes/Musica/Img.jsx
+++ src/Componentes/Musica/Img.jsx
@@ -1,29 +1,26 @@
-import { useState, useRef } from 'react';
-import YouTube from 'react-youtube';
-import { BarSound } from './BarSound';
-import { ListChannel } from './ListChannel/ListChannel';
+import { useState, useRef } from "react";
+import YouTube from "react-youtube";
+import { BarSound } from "./BarSound";
+import { ListChannel } from "./ListChannel/ListChannel";
 
 export const ImagenMusic = () => {
-  const [youtubeID, setYoutubeID] = useState('jfKfPfyJRdk');
-  const [imgID, setImgID] = useState('0hjOGLFaQa0AAAAd/lofi-girl-lofi');
+  const [youtubeID, setYoutubeID] = useState("jfKfPfyJRdk");
+  const [imgID, setImgID] = useState("0hjOGLFaQa0AAAAd/lofi-girl-lofi");
   const [play, setPlay] = useState(false);
- 
+  const [volume, setVolume] = useState(100);
   const playerRef = useRef(null);
-
   const onReady = (event) => {
     playerRef.current = event.target;
     playerRef.current.playVideo();
   };
-
-  
   return (
     <>
       <div className="items-center w-40 h-20">
         <YouTube
           videoId={youtubeID}
-          opts={{ playerVars: { autoplay: play ? 1 : 0} }}
+          opts={{ playerVars: { autoplay: play ? 1 : 0 } }}
           onReady={onReady}
-          className='hidden'
+          className="hidden"
         />
         <img
           src={`https://media.tenor.com/${imgID}.gif`}
@@ -33,10 +30,12 @@ export const ImagenMusic = () => {
       <div className="flex flex-col w-full items-center justify-center gap-4">
         <div className="flex flex-col items-center gap-1">
           <span className="font-bold">Lofi song</span>
-          <ListChannel setYoutubeID={setYoutubeID} setImgID={setImgID} />
+          <ListChannel setYoutubeID={setYoutubeID} setImgID={setImgID}  setVolume={setVolume} />
         </div>
         <div className="flex gap-2">
           <BarSound
+            volume={volume}
+            setVolume={setVolume}
             togglePlay={setPlay}
             setYoutubeID={setYoutubeID}
             setImgID={setImgID}
