349b8928844b42926d0689c874ff5d0262b63ef6
kevin-dorado <kevindorado2811@gmail.com>
2023-10-20 12:19:43 -0300

    check ToDo Design

--- src/utilities/Task.jsx
+++ src/utilities/Task.jsx
@@ -1,15 +1,22 @@
 import { FaCheck, FaTrash } from 'react-icons/fa';
+import { CheckCircle2, Circle, X } from 'lucide-react';
+import { useState } from 'react';
 export const Task = ({text,id,deleteToDo}) =>{
 
+    const[circle, setCircle] = useState(false)
+
     return(
-    <li className="flex justify-between items-center p-4 bg-white shadow rounded-md mb-4 mx-1">
-        <span>{text}</span>
-        <div>
-            <button id={id+"check"} className="text-green-600 hover:text-green-800 mr-2">
-                <FaCheck />
+    <li className="flex  items-center p-4 py-4 bg-white shadow rounded-md mb-4 mx-1">
+        <div className='flex items-center'>
+            <button  onMouseEnter={()=> setCircle(true)}  onMouseLeave={() => setCircle(false)} id={id+"check"} className="text-[#467BD5] pr-4">
+                 {circle ?  <CheckCircle2 />  :  <Circle />}   
             </button>
+        </div>
+        
+        <div className=' flex justify-between w-full'>
+            <span>{text}</span>
             <button id={id+"trash"} onClick={()=>deleteToDo(id)} className="text-red-600 hover:text-red-800">
-                <FaTrash />
+            <X  strokeWidth={1.25} />
             </button>
         </div>
     </li>
